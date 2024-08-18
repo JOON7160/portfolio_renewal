48b376f851acd587e5ff567814b602b092a4a9d3
JOON617 <jjyt2525@gmail.com>
2024-08-18 15:55:50 +0900

    typing 효과 퍼블리싱

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
+//typingEffect
+// 텍스트 요소
+const typingTxt = document.querySelector('.typing-ani');
+//글자 모음
+const letters = ["Basic", "Foundation","Fast"];
+//글자 입력 속도
+const speed = 100;
+//현재 지정된 글자
+let i = 0;
+
+//delay 기능(마이크로초)
+function delay(ms) {
+    return new Promise(res => setTimeout(res,ms))
+}
+//타이핑 기능
+const typing = async () => {
+    const letter = letters[i].split("");
+    while (letter.length) {
+        //글자 속도 조절
+        await delay(speed);
+        typingTxt.innerHTML += letter.shift();
+    }
+    //작성 된 후 딜레이
+    await delay(2000);
+    remove();
+}
+const remove = async () => {
+    const letter = letters[i].split("");
+    while (letter.length) {
+        await delay(speed);
+        letter.pop();
+        typingTxt.innerHTML = letter.join("");
+    }
+    i = !letters [ i + 1 ] ? 0 : i + 1;
+    typing();
+}
+setTimeout(typing, 1500)
+
 //digital clock
 let clockHours = document.querySelector('.hours');
 let clockMinutes = document.querySelector('.minutes');
